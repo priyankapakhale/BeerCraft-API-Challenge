a36fc5f81f4d928f4fffc0d45d5207b7d4ba7a21
priyanka <priyanka@Zedex-Infos-Macbook-Pro.local>
2020-11-21 04:07:09 +0530

    changed search bar

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { connect } from 'react-redux'
 import { searchBeerByName } from '../redux/actions/actions'
+import { Form } from 'react-bootstrap'
 
 const SearchBeer = (props) => {
   const [name, setName] = useState('')
@@ -13,7 +14,9 @@ const SearchBeer = (props) => {
   return (
     <div className="d-flex justify-content-center">
       <div className="col-lg-6 col-md-8 col-12">
-        <input className="px-3 py-1 mr-3" type="text" placeholder="Enter beer's name" value={name} onChange={e => setName(e.target.value)} />
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Control className="px-3 py-1 mr-3" type="text" placeholder="Enter beer's name" value={name} onChange={e => setName(e.target.value)}  />
+        </Form.Group>
       </div>
     </div>
   );
